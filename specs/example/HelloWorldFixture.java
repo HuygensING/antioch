182538b2829af1108aa181247cf40511d81ae0dc
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-01-28 15:04:35 +0100

    * add concordion example

---
+++ specs/example/HelloWorldFixture.java
@@ -0,0 +1,11 @@
+package example;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class HelloWorldFixture {
+  public String getGreeting() {
+    return "Hello World!";
+  }
+}
