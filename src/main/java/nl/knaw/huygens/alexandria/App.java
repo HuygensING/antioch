0be676eef2e1b63efaca6139f74dc1db65afc686
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-01-28 13:42:38 +0100

    Initial Maven directory hierarchy

---
+++ src/main/java/nl/knaw/huygens/alexandria/App.java
@@ -0,0 +1,13 @@
+package nl.knaw.huygens.alexandria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
