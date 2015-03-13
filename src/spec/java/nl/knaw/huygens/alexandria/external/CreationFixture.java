bcf8d515358a9ab2e07793ccad393ab23c319841
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-03-13 13:45:37 +0100

    Fix remove testing remnant

--- src/spec/java/nl/knaw/huygens/alexandria/external/CreationFixture.java
+++ src/spec/java/nl/knaw/huygens/alexandria/external/CreationFixture.java
@@ -1,27 +1,23 @@
 package nl.knaw.huygens.alexandria.external;
 
-import static org.mockito.Matchers.anyObject;
-
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
-import nl.knaw.huygens.alexandria.exception.IllegalResourceException;
+import nl.knaw.huygens.alexandria.model.AlexandriaResource;
 import nl.knaw.huygens.alexandria.util.UUIDParser;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 
 @RunWith(ConcordionRunner.class)
 public class CreationFixture extends ResourcesFixture {
   @Override
   public void request(String method, String path) {
-//    when(resourceService().createResource(any(AlexandriaResource.class))).then(returnsFirstArg());
     System.err.println(String.format("CreationFixture.request(%s,%s)", method, path));
 
-    Mockito.when(resourceService().createResource(anyObject()))
-           .thenThrow(new IllegalResourceException(Response.status(Status.BAD_REQUEST).entity("asjemenou!").build()));
+    when(resourceService().createResource(any(AlexandriaResource.class))).then(returnsFirstArg());
 
     super.request(method, path);
   }
