80c74ff85daaa7b8ec3e8f44874c7e1aec60a1ed
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-03-16 09:14:28 +0100

    More tests for UUIDParam

--- src/test/java/nl/knaw/huygens/alexandria/endpoint/param/UUIDParamTest.java
+++ src/test/java/nl/knaw/huygens/alexandria/endpoint/param/UUIDParamTest.java
@@ -1,18 +1,34 @@
 package nl.knaw.huygens.alexandria.endpoint.param;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.ws.rs.WebApplicationException;
+import java.util.UUID;
 
 import org.junit.Test;
 
 public class UUIDParamTest {
-  @Test
-  public void testConstructorAcceptsValidUUID() {
-    new UUIDParam("ea71773e-cbaf-11e4-a5af-473cb94dfabd");
-  }
+  private static final String A_VALID_UUID = "ea71773e-cbaf-11e4-a5af-473cb94dfabd";
 
   @Test(expected = WebApplicationException.class)
   public void testConstructorRejectsInvalidUUID() {
     new UUIDParam("invalid-uuid");
   }
 
+  @Test
+  public void testConstructorAcceptsValidUUID() {
+    new UUIDParam(A_VALID_UUID);
+  }
+
+  @Test
+  public void testParamYieldsOriginalValue() {
+    final String expected = A_VALID_UUID;
+    assertEquals(expected, new UUIDParam(A_VALID_UUID).getOriginalParam());
+  }
+
+  @Test
+  public void testParamIsConvertedToValue() {
+    final UUID expected = UUID.fromString(A_VALID_UUID);
+    assertEquals(expected, new UUIDParam(A_VALID_UUID).getValue());
+  }
 }
