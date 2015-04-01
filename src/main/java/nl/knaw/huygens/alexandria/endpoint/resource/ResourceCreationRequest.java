8e9a5cb05c628b8f2d58a5bda77bcfb4a47d1afd
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-04-01 10:28:37 +0200

    Added ResourceCreationRequest (incoming JSON for creating a new Resource)

---
+++ src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourceCreationRequest.java
@@ -0,0 +1,34 @@
+package nl.knaw.huygens.alexandria.endpoint.resource;
+
+import java.util.Optional;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import nl.knaw.huygens.alexandria.endpoint.InstantParam;
+import nl.knaw.huygens.alexandria.endpoint.UUIDParam;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+@JsonTypeName("resource")
+public class ResourceCreationRequest {
+  private UUIDParam id;
+  private String ref;
+  private InstantParam createdOn;
+  private Set<UUIDParam> annotations;
+
+  public Optional<UUIDParam> getId() {
+    return Optional.ofNullable(id);
+  }
+
+  public Optional<String> getRef() {
+    return Optional.ofNullable(ref);
+  }
+
+  public Optional<InstantParam> getCreatedOn() {
+    return Optional.ofNullable(createdOn);
+  }
+
+  public Optional<Set<UUIDParam>> getAnnotations() {
+    return Optional.ofNullable(annotations);
+  }
+}
