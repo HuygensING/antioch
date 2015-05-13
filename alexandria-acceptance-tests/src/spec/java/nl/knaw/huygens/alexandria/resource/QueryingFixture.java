bbf87553bab4b198acd346e5d383eee4a3798716
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-05-13 16:29:34 +0200

    comment out parts that don't compile

--- alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/resource/QueryingFixture.java
+++ alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/resource/QueryingFixture.java
@@ -7,27 +7,32 @@ import java.util.UUID;
 
 import nl.knaw.huygens.alexandria.exception.NotFoundException;
 import nl.knaw.huygens.alexandria.model.AlexandriaAnnotation;
+import nl.knaw.huygens.alexandria.model.AlexandriaAnnotationBody;
 import nl.knaw.huygens.alexandria.model.AlexandriaResource;
+import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
 
 public class QueryingFixture extends ResourceFixture {
-  private AlexandriaResource resource;
+	private AlexandriaResource resource;
 
-  public void existingResource(String id) {
-    UUID uuid = UUID.fromString(id);
-    resource = new AlexandriaResource(uuid, Instant.now());
-    when(resourceService().readResource(uuid)).thenReturn(resource);
-  }
+	public void existingResource(String id) {
+		UUID uuid = UUID.fromString(id);
+		TentativeAlexandriaProvenance provenance = new TentativeAlexandriaProvenance("who", Instant.now(), "why");
+		resource = new AlexandriaResource(uuid, provenance);
+		when(resourceService().readResource(uuid)).thenReturn(resource);
+	}
 
-  public void withReference(String reference) {
-    resource.setRef(reference);
-  }
+	public void withReference(String reference) {
+		resource.setRef(reference);
+	}
 
-  public void withAnnotation(String id) {
-    resource.addAnnotation(new AlexandriaAnnotation(UUID.fromString(id), "<type>", "<value>"));
-  }
+	public void withAnnotation(String id) {
+		TentativeAlexandriaProvenance provenance = new TentativeAlexandriaProvenance("who", Instant.now(), "why");
+		AlexandriaAnnotationBody body = new AlexandriaAnnotationBody(UUID.fromString(id), "<type>", "<value>", provenance);
+		resource.addAnnotation(new AlexandriaAnnotation(UUID.fromString(id), body, provenance));
+	}
 
-  public void noSuchResource(String id) {
-    UUID uuid = UUID.fromString(id);
-    when(resourceService().readResource(uuid)).thenThrow(new NotFoundException());
-  }
+	public void noSuchResource(String id) {
+		UUID uuid = UUID.fromString(id);
+		when(resourceService().readResource(uuid)).thenThrow(new NotFoundException());
+	}
 }
