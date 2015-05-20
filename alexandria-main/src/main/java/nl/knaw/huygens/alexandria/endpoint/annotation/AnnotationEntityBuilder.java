8681cc42de97fb1f90f1cf2a462fba3df3709c13
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-05-20 12:10:33 +0200

    Bind EntityBuilder classes as Singleton, cleaning up static instance creation clutter

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/annotation/AnnotationEntityBuilder.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/annotation/AnnotationEntityBuilder.java
@@ -1,17 +1,21 @@
 package nl.knaw.huygens.alexandria.endpoint.annotation;
 
+import javax.inject.Inject;
+
 import nl.knaw.huygens.alexandria.config.AlexandriaConfiguration;
 import nl.knaw.huygens.alexandria.endpoint.AnnotationEntity;
 import nl.knaw.huygens.alexandria.model.AlexandriaAnnotation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationEntityBuilder {
+  private final Logger LOG = LoggerFactory.getLogger(AnnotationEntityBuilder.class);
+
   private final AlexandriaConfiguration config;
 
-  public static AnnotationEntityBuilder forConfig(AlexandriaConfiguration config) {
-    return new AnnotationEntityBuilder(config);
-  }
-
-  private AnnotationEntityBuilder(AlexandriaConfiguration config) {
+  @Inject
+  public AnnotationEntityBuilder(AlexandriaConfiguration config) {
+    LOG.trace("AnnotationEntityBuilder created: config=[{}]", config);
     this.config = config;
   }
 
