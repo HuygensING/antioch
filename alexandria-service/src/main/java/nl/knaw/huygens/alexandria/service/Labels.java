830e1bf1af7708653d3191cae4500b82cf27c480
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-04 11:21:49 +0200

    add service module with tinkerpop dependencies

---
+++ alexandria-service/src/main/java/nl/knaw/huygens/alexandria/service/Labels.java
@@ -0,0 +1,5 @@
+package nl.knaw.huygens.alexandria.service;
+
+public enum Labels {
+  Resource, Annotation, AnnotationBody, Provenance
+}
---
+++ alexandria-service/src/main/java/nl/knaw/huygens/alexandria/service/TinkerpopAlexandriaService.java
@@ -0,0 +1,68 @@
+package nl.knaw.huygens.alexandria.service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import nl.knaw.huygens.alexandria.model.AlexandriaAnnotation;
+import nl.knaw.huygens.alexandria.model.AlexandriaAnnotationBody;
+import nl.knaw.huygens.alexandria.model.AlexandriaResource;
+import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
+
+public class TinkerpopAlexandriaService implements AlexandriaService {
+
+  @Override
+  public boolean createOrUpdateResource(UUID uuid, String ref, TentativeAlexandriaProvenance provenance) {
+    boolean newlyCreated = true;
+    return newlyCreated;
+  }
+
+  @Override
+  public AlexandriaResource readResource(UUID uuid) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean createAnnotationBody(UUID uuid, Optional<String> type, String value, TentativeAlexandriaProvenance provenance) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public AlexandriaAnnotationBody findAnnotationBodyWithTypeAndValue(Optional<String> type, String value) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public AlexandriaAnnotationBody readAnnotationBody(UUID uuid) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public AlexandriaAnnotation annotate(AlexandriaResource resource, AlexandriaAnnotationBody annotationbody, TentativeAlexandriaProvenance provenance) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public AlexandriaAnnotation annotate(AlexandriaAnnotation annotation, AlexandriaAnnotationBody annotationbody, TentativeAlexandriaProvenance provenance) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public AlexandriaAnnotation readAnnotation(UUID uuid) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Set<AlexandriaResource> readSubResources(UUID uuid) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
