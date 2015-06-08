276f1de17259ac7ed17841ddac478123221c739b
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-08 16:57:56 +0200

    Add state

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourcePrototype.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourcePrototype.java
@@ -1,35 +1,44 @@
 package nl.knaw.huygens.alexandria.endpoint.resource;
 
 import java.util.Optional;
-
 import javax.validation.constraints.NotNull;
-
 import nl.knaw.huygens.alexandria.endpoint.InstantParam;
 import nl.knaw.huygens.alexandria.endpoint.UUIDParam;
-
+import nl.knaw.huygens.alexandria.model.AlexandriaState;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
 @JsonTypeName("resource")
 public class ResourcePrototype {
-	private UUIDParam id;
+  private UUIDParam id;
 
-	@NotNull(message = "{nl.knaw.huygens.alexandria.endpoint.resource.ResourceProtoType.ref.NotNull.message}")
-	private String ref;
+  @NotNull(message = "{nl.knaw.huygens.alexandria.endpoint.resource.ResourceProtoType.ref.NotNull.message}")
+  private String ref;
 
-	private InstantParam createdOn;
+  private InstantParam createdOn;
+  private AlexandriaState state;
 
-	public UUIDParam getId() {
-		return id;
-	}
+  public UUIDParam getId() {
+    return id;
+  }
 
-	public String getRef() {
-		return ref;
-	}
+  public String getRef() {
+    return ref;
+  }
 
-	public Optional<InstantParam> getCreatedOn() {
-		return Optional.ofNullable(createdOn);
-	}
+  public AlexandriaState getState() {
+    return state;
+  }
+
+  public Optional<InstantParam> getCreatedOn() {
+    return Optional.ofNullable(createdOn);
+  }
+
+  @JsonIgnore
+  public void setState(AlexandriaState state) {
+    this.state = state;
+  }
 
 }
