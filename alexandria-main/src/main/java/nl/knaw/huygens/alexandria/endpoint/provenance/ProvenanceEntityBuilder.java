4164fb6d150598e83a6c2bd4b80f07a590c2ba91
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-09 16:24:59 +0200

    implented ProvenanceEntity + Builder

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/provenance/ProvenanceEntityBuilder.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/provenance/ProvenanceEntityBuilder.java
@@ -1,11 +1,22 @@
 package nl.knaw.huygens.alexandria.endpoint.provenance;
 
+import javax.inject.Inject;
+
+import nl.knaw.huygens.Log;
+import nl.knaw.huygens.alexandria.config.AlexandriaConfiguration;
 import nl.knaw.huygens.alexandria.model.AlexandriaProvenance;
 
 public class ProvenanceEntityBuilder {
+  private final AlexandriaConfiguration config;
+
+  @Inject
+  public ProvenanceEntityBuilder(AlexandriaConfiguration config) {
+    Log.trace("ProvenanceEntityBuilder created: config=[{}]", config);
+    this.config = config;
+  }
 
   public ProvenanceEntity build(AlexandriaProvenance provenance) {
-    return null;
+    return ProvenanceEntity.of(provenance).withConfig(config);
   }
 
 }
