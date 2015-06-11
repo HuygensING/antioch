8443ec11479390cc9f2f91bf61cbfef64520c9d6
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-11 14:51:38 +0200

    go back to the com.tinkerpop / 3.0.0M7 version, since it's compatible with peapod 0.1.0 Use peapod as tinkerpop ogm

--- alexandria-service/src/main/java/nl/knaw/huygens/alexandria/frames/AlexandriaResourceVF.java
+++ alexandria-service/src/main/java/nl/knaw/huygens/alexandria/frames/AlexandriaResourceVF.java
@@ -1,81 +1,30 @@
 package nl.knaw.huygens.alexandria.frames;
 
-import java.util.UUID;
-import nl.knaw.huygens.alexandria.model.AlexandriaProvenance;
-import nl.knaw.huygens.alexandria.model.AlexandriaResource;
-import nl.knaw.huygens.alexandria.model.AlexandriaState;
-import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
+import java.time.Instant;
+
 import nl.knaw.huygens.alexandria.service.Labels;
+import peapod.annotations.Vertex;
 
-public class AlexandriaResourceVF extends AbstractVertexFrame {
+@Vertex(Labels.RESOURCE)
+public abstract class AlexandriaResourceVF {
+  public abstract String getRef();
 
-  public static final Labels LABEL = Labels.Resource;
+  public abstract void setRef(String ref);
 
-  enum Properties {
-    uuid, ref, state, //
-    provenanceWho, provenanceWhen, provenanceWhy
-  }
+  public abstract String getId();
 
-  public UUID getUUID() {
-    return UUID.fromString(getProperty(Properties.uuid.name()));
-  }
+  public abstract void setId(String id);
 
-  public void setUUID(UUID uuid) {
-    setProperty(Properties.uuid.name(), uuid.toString());
-  }
+  public abstract String getProvenanceWho();
 
-  public String getRef() {
-    return getProperty(Properties.ref.name());
-  }
+  public abstract void setProvenanceWho(String who);
 
-  public void setRef(String ref) {
-    setProperty(Properties.ref.name(), ref);
-  }
+  public abstract Instant getProvenanceWhen();
 
-  public AlexandriaState getState() {
-    return AlexandriaState.valueOf(getProperty(Properties.state.name()));
-  }
+  public abstract void setProvenanceWhen(Instant when);
 
-  public void setState(AlexandriaState state) {
-    setProperty(Properties.state.name(), state.name());
-  }
+  public abstract String getProvenanceWhy();
 
-  public TentativeAlexandriaProvenance getProvenance() {
-    return new TentativeAlexandriaProvenance(//
-        getProperty(Properties.provenanceWho.name()),//
-        getProperty(Properties.provenanceWhen.name()),//
-        getProperty(Properties.provenanceWhy.name())//
-    );
-  }
-
-  public void setProvenance(AlexandriaProvenance provenance) {
-    setProperty(Properties.provenanceWhen.name(), provenance.getWhen());
-    setProperty(Properties.provenanceWho.name(), provenance.getWho());
-    setProperty(Properties.provenanceWhy.name(), provenance.getWhy());
-  }
-
-  // public void addAnnotation(AlexandriaAnnotation annotation) {
-  // r.addAnnotation(annotation);
-  // }
-  //
-  // public Set<AlexandriaAnnotation> getAnnotations() {
-  // return r.getAnnotations();
-  // }
-
-  public static AlexandriaResourceVF of(AlexandriaResource resource) {
-    AlexandriaResourceVF vf = new AlexandriaResourceVF();
-    vf.setUUID(resource.getId());
-    vf.setProvenance(resource.getProvenance());
-    vf.setRef(resource.getRef());
-    vf.setState(resource.getState());
-    return vf;
-  }
-
-  public AlexandriaResource get() {
-    AlexandriaResource ar = new AlexandriaResource(getUUID(), getProvenance());
-    ar.setRef(getRef());
-    ar.setState(getState());
-    return ar;
-  }
+  public abstract void setProvenanceWhy(String why);
 
 }
--- alexandria-service/src/main/java/nl/knaw/huygens/alexandria/storage/Storage.java
+++ alexandria-service/src/main/java/nl/knaw/huygens/alexandria/storage/Storage.java
@@ -1,9 +1,20 @@
 package nl.knaw.huygens.alexandria.storage;
 
 import java.util.UUID;
+
+import nl.knaw.huygens.alexandria.frames.AlexandriaResourceVF;
+import nl.knaw.huygens.alexandria.model.AlexandriaProvenance;
 import nl.knaw.huygens.alexandria.model.AlexandriaResource;
+import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
+import peapod.FramedGraph;
+
+import com.tinkerpop.gremlin.structure.Graph;
+import com.tinkerpop.gremlin.structure.Transaction;
+import com.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 public class Storage {
+  Graph g = TinkerGraph.open();
+  FramedGraph fg = new FramedGraph(g, Package.getPackage("nl.knaw.huygens.alexandria.frames"));
 
   public boolean exists(Class clazz, UUID uuid) {
     return false;
@@ -11,14 +22,30 @@ public class Storage {
 
   public AlexandriaResource read(Class clazz, UUID uuid) {
     if (clazz.equals(AlexandriaResource.class)) {
-      Storable<AlexandriaResource> sar = new AlexandriaResourceStorable();
+      Transaction tx = fg.tx();
+      AlexandriaResourceVF arvf = fg.v(uuid.toString());
+      tx.close();
+      TentativeAlexandriaProvenance provenance = new TentativeAlexandriaProvenance(arvf.getProvenanceWho(), arvf.getProvenanceWhen(), arvf.getProvenanceWhy());
+      AlexandriaResource resource = new AlexandriaResource(uuid, provenance);
+      return resource;
     }
     return null;
   }
 
   public void createOrUpdate(AlexandriaResource resource) {
-    Storable<AlexandriaResource> sar = AlexandriaResourceStorable.of(resource);
-
+    AlexandriaResourceVF arvf = null;
+    Transaction tx = fg.tx();
+    if (exists(resource.getClass(), resource.getId())) {
+      arvf = fg.v(resource.getId());
+    } else {
+      arvf = fg.addVertex(AlexandriaResourceVF.class, resource.getId());
+    }
+    arvf.setId(resource.getId().toString());
+    AlexandriaProvenance provenance = resource.getProvenance();
+    arvf.setProvenanceWhen(provenance.getWhen());
+    arvf.setProvenanceWho(provenance.getWho());
+    arvf.setProvenanceWhy(provenance.getWhy());
+    arvf.setRef(resource.getRef());
+    tx.commit();
   }
-
 }
