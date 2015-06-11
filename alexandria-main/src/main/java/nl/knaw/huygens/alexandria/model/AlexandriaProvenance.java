85dc5cc81f69965e090ba29c499ed1993f948263
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-11 17:35:53 +0200

    First working test for peapod framegraph/AlexandriaResource storage

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/model/AlexandriaProvenance.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/model/AlexandriaProvenance.java
@@ -33,4 +33,5 @@ public class AlexandriaProvenance {
   public String getWhy() {
     return why;
   }
+
 }
--- alexandria-service/src/main/java/nl/knaw/huygens/alexandria/storage/Storage.java
+++ alexandria-service/src/main/java/nl/knaw/huygens/alexandria/storage/Storage.java
@@ -2,19 +2,28 @@ package nl.knaw.huygens.alexandria.storage;
 
 import java.util.UUID;
 
-import nl.knaw.huygens.alexandria.frames.AlexandriaResourceVF;
+import jline.internal.Log;
 import nl.knaw.huygens.alexandria.model.AlexandriaProvenance;
 import nl.knaw.huygens.alexandria.model.AlexandriaResource;
 import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
+import nl.knaw.huygens.alexandria.storage.frames.AlexandriaResourceVF;
 import peapod.FramedGraph;
 
 import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.structure.Transaction;
-import com.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 public class Storage {
-  Graph g = TinkerGraph.open();
-  FramedGraph fg = new FramedGraph(g, Package.getPackage("nl.knaw.huygens.alexandria.frames"));
+  private static Graph g;
+  private static FramedGraph fg;
+  private static boolean transactionsSupported;
+
+  private Transaction tx;
+
+  public Storage(Graph graph) {
+    g = graph;
+    transactionsSupported = g.features().graph().supportsTransactions();
+    fg = new FramedGraph(g, AlexandriaResourceVF.class.getPackage());
+  }
 
   public boolean exists(Class clazz, UUID uuid) {
     return false;
@@ -22,9 +31,9 @@ public class Storage {
 
   public AlexandriaResource read(Class clazz, UUID uuid) {
     if (clazz.equals(AlexandriaResource.class)) {
-      Transaction tx = fg.tx();
-      AlexandriaResourceVF arvf = fg.v(uuid.toString());
-      tx.close();
+      startTransaction();
+      AlexandriaResourceVF arvf = fg.V(AlexandriaResourceVF.class).has("id", uuid.toString()).toList().get(0);
+      commitTransaction();
       TentativeAlexandriaProvenance provenance = new TentativeAlexandriaProvenance(arvf.getProvenanceWho(), arvf.getProvenanceWhen(), arvf.getProvenanceWhy());
       AlexandriaResource resource = new AlexandriaResource(uuid, provenance);
       return resource;
@@ -33,8 +42,8 @@ public class Storage {
   }
 
   public void createOrUpdate(AlexandriaResource resource) {
+    startTransaction();
     AlexandriaResourceVF arvf = null;
-    Transaction tx = fg.tx();
     if (exists(resource.getClass(), resource.getId())) {
       arvf = fg.v(resource.getId());
     } else {
@@ -46,6 +55,29 @@ public class Storage {
     arvf.setProvenanceWho(provenance.getWho());
     arvf.setProvenanceWhy(provenance.getWhy());
     arvf.setRef(resource.getRef());
-    tx.commit();
+    commitTransaction();
+  }
+
+  // private methods
+  private void startTransaction() {
+    if (transactionsSupported) {
+      tx = fg.tx();
+    }
+  }
+
+  private void commitTransaction() {
+    if (transactionsSupported) {
+      tx.commit();
+      tx.close();
+    }
+  }
+
+  private void rollbackTransaction() {
+    if (transactionsSupported) {
+      tx.rollback();
+      tx.close();
+    } else {
+      Log.error("rollback called, but transactions are not supported by graph {}", g);
+    }
   }
 }
