62d6a45deeb75cb7198b27396970201cc196945d
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-12 14:43:00 +0200

    add project.version + builddate to /about

--- alexandria-server/src/main/java/nl/knaw/huygens/alexandria/endpoint/AboutEndpoint.java
+++ alexandria-server/src/main/java/nl/knaw/huygens/alexandria/endpoint/AboutEndpoint.java
@@ -1,8 +1,10 @@
 package nl.knaw.huygens.alexandria.endpoint;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.PropertyResourceBundle;
 
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
@@ -24,8 +26,25 @@ public class AboutEndpoint extends JSONEndpoint {
   @GET
   public Response getMetadata() {
     Map<String, String> data = Maps.newLinkedHashMap();
+    data.put("version", getProperty("version"));
+    data.put("builddate", getProperty("builddate"));
     data.put("started", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(this.starttime));
     return Response.ok(data).build();
+  }
+
+  // private methods
+  private static PropertyResourceBundle propertyResourceBundle;
+
+  private static synchronized String getProperty(String key) {
+    if (propertyResourceBundle == null) {
+      try {
+        propertyResourceBundle = new PropertyResourceBundle(//
+            Thread.currentThread().getContextClassLoader().getResourceAsStream("about.properties"));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return propertyResourceBundle.getString(key);
   }
 
 }
