8fc3862e40d4daa26bc5cba0c2848a46bf9a3fa6
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-17 16:55:14 +0200

    added TODO to make this work more for more than just the first call

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/MatchesPathId.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/MatchesPathId.java
@@ -1,20 +1,22 @@
 package nl.knaw.huygens.alexandria.endpoint.resource;
 
-import javax.validation.Constraint;
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-import javax.validation.Payload;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.PathParam;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.UUID;
 
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.PathParam;
+
+import nl.knaw.huygens.Log;
 import nl.knaw.huygens.alexandria.endpoint.UUIDParam;
 
-@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = MatchesPathId.Validator.class)
 public @interface MatchesPathId {
@@ -24,6 +26,7 @@ public @interface MatchesPathId {
 
   Class<? extends Payload>[] payload() default {};
 
+  // TODO fix this validator, it now only works once, then the paramId is set, the next @MatchesPathId doesn't make a new Validator, so paramId is not updated
   class Validator implements ConstraintValidator<MatchesPathId, ResourcePrototype> {
     final UUID paramId;
 
@@ -34,6 +37,7 @@ public @interface MatchesPathId {
     @Override
     public void initialize(MatchesPathId constraintAnnotation) {
       // nothing needed.
+      Log.trace("MatchesPathId.initialize");
     }
 
     @Override
@@ -46,4 +50,5 @@ public @interface MatchesPathId {
       return protoId.equals(paramId);
     }
   }
+
 }
