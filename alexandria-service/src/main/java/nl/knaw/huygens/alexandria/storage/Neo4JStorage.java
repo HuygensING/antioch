24ceb1eb508dfbe6f8783476c4d35f674ab81088
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-22 11:02:58 +0200

    Turn off neo4jstorage until neo4j-gremlin is back in the org.apache.tinkerpop family

--- alexandria-service/src/main/java/nl/knaw/huygens/alexandria/storage/Neo4JStorage.java
+++ alexandria-service/src/main/java/nl/knaw/huygens/alexandria/storage/Neo4JStorage.java
@@ -1,16 +1,10 @@
 package nl.knaw.huygens.alexandria.storage;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
 
-import nl.knaw.huygens.alexandria.config.AlexandriaConfiguration;
-
-import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
-
-@Singleton
-public class Neo4JStorage extends Storage {
-  @Inject
-  public Neo4JStorage(AlexandriaConfiguration configuration) {
-    super(Neo4jGraph.open(configuration.getStorageDirectory()));
-  }
-}
+//@Singleton
+//public class Neo4JStorage extends Storage {
+//  @Inject
+//  public Neo4JStorage(AlexandriaConfiguration configuration) {
+//    super(Neo4jGraph.open(configuration.getStorageDirectory()));
+//  }
+// }
