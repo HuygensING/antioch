a0d8283b6a5b84947230b131783f109b98f4c09d
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-06-22 11:40:44 +0200

    Simplify MatchedsPathIdValidatorTest by making it a parameterized test

--- alexandria-main/src/test/java/nl/knaw/huygens/alexandria/endpoint/resource/MatchesPathIdValidatorTest.java
+++ alexandria-main/src/test/java/nl/knaw/huygens/alexandria/endpoint/resource/MatchesPathIdValidatorTest.java
@@ -1,55 +1,45 @@
 package nl.knaw.huygens.alexandria.endpoint.resource;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import javax.ws.rs.core.PathSegment;
 import javax.ws.rs.core.UriInfo;
 
 import com.google.common.collect.Lists;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import nl.knaw.huygens.alexandria.endpoint.UUIDParam;
 import nl.knaw.huygens.alexandria.endpoint.resource.MatchesPathId.Validator;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class MatchesPathIdValidatorTest {
-  private UriInfo mockedUriInfo;
-  private PathSegment mockedPathSegment;
-  private ResourcePrototype mockedPrototype;
-  private Validator validator;
-
-  @Before
-  public void setup() {
-    mockedUriInfo = mock(UriInfo.class);
-    mockedPathSegment = mock(PathSegment.class);
-    mockedPrototype = mock(ResourcePrototype.class);
-    validator = new Validator(mockedUriInfo);
-  }
 
   @Test
-  public void differentIdInURIAndPrototypeShouldBeInvalid() throws Exception {
-    when(mockedPrototype.getId()).thenReturn(new UUIDParam(someUUID()));
+  @Parameters({ //
+      "fe944b98-18b1-11e5-9d51-1f35c61c7a23, fe944b98-18b1-11e5-9d51-1f35c61c7a23, true", // same UUIDs
+      "fe944b98-18b1-11e5-9d51-1f35c61c7a23, 27947b26-18b2-11e5-be1c-c7fb9bd13f9b, false" // different UUIDs
+  })
+  public void testValidationOfUUIDinURIShouldMatchUUIDinPrototype(String uriId, String protoId, boolean valid) {
+    final PathSegment mockedPathSegment = mock(PathSegment.class);
+    when(mockedPathSegment.getPath()).thenReturn(protoId);
+
+    final UriInfo mockedUriInfo = mock(UriInfo.class);
     when(mockedUriInfo.getPathSegments()).thenReturn(Lists.newArrayList(mockedPathSegment));
-    when(mockedPathSegment.getPath()).thenReturn(anotherUUID());
 
-    Assert.assertFalse(validator.isValid(mockedPrototype, null));
+    final ResourcePrototype mockedPrototype = mock(ResourcePrototype.class);
+    when(mockedPrototype.getId()).thenReturn(new UUIDParam(uriId));
+
+    assertThat(new Validator(mockedUriInfo).isValid(mockedPrototype, null), is(valid));
+
+    verify(mockedUriInfo, atLeastOnce()).getPathSegments();
+    verify(mockedPrototype).getId();
   }
 
-  @Test
-  public void sameIdInURIAndPrototypeShouldBeValid() throws Exception {
-    when(mockedPrototype.getId()).thenReturn(new UUIDParam(someUUID()));
-    when(mockedUriInfo.getPathSegments()).thenReturn(Lists.newArrayList(mockedPathSegment));
-    when(mockedPathSegment.getPath()).thenReturn(someUUID());
-
-    Assert.assertTrue(validator.isValid(mockedPrototype, null));
-  }
-
-  private String someUUID() {
-    return "fe944b98-18b1-11e5-9d51-1f35c61c7a23";
-  }
-
-  private String anotherUUID() {
-    return "27947b26-18b2-11e5-be1c-c7fb9bd13f9b";
-  }
 }
