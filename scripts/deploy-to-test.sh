88276273f8b4a0b84156965cc7b9718aca06800c
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-06-30 09:51:48 +0200

    run deploy from root module, so a mvn install is not required

--- scripts/deploy-to-test.sh
+++ scripts/deploy-to-test.sh
@@ -5,8 +5,7 @@ function divider {
 }
 
 base=http://demo17.huygens.knaw.nl/test-alexandria
-cd alexandria-webapp && \
-mvn package tomcat:redeploy -P test
+mvn package tomcat:redeploy -P test -pl alexandria-webapp -am
 divider
 sleep 20 # wait for the server to boot
 curlCmd="curl -sSf $base/about"
