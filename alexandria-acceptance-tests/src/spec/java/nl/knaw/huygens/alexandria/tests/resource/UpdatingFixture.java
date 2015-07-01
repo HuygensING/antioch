950a43226846d0a3704755e69c7a3bf93505c415
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-07-01 18:00:21 +0200

    set state=Temporary on resources created via POST

--- alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/tests/resource/UpdatingFixture.java
+++ alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/tests/resource/UpdatingFixture.java
@@ -5,19 +5,20 @@ import static org.mockito.Mockito.when;
 
 import java.util.UUID;
 
-import nl.knaw.huygens.alexandria.exception.NotFoundException;
-import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
-
 import org.concordion.api.ExpectedToFail;
+
+import nl.knaw.huygens.alexandria.exception.NotFoundException;
+import nl.knaw.huygens.alexandria.model.AlexandriaState;
+import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
 
 @ExpectedToFail
 public class UpdatingFixture extends ResourceFixture {
-	@Override
-	public void request(String method, String path) {
-		when(service().readResource(any(UUID.class))).thenThrow(new NotFoundException());
-		when(service().createOrUpdateResource(any(UUID.class), any(String.class), any(TentativeAlexandriaProvenance.class))).thenReturn(false);
+  @Override
+  public void request(String method, String path) {
+    when(service().readResource(any(UUID.class))).thenThrow(new NotFoundException());
+    when(service().createOrUpdateResource(any(UUID.class), any(String.class), any(TentativeAlexandriaProvenance.class), any(AlexandriaState.class))).thenReturn(false);
 
-		super.request(method, path);
-	}
+    super.request(method, path);
+  }
 
 }
