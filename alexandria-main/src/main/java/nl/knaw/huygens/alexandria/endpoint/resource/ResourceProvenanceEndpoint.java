7f3d42e574584cfeaa028acad9f45a4fcdd70df4
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-07-01 14:18:06 +0200

    throw correct error for /resources/{non-existing-uuid}/provenance

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourceProvenanceEndpoint.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourceProvenanceEndpoint.java
@@ -1,10 +1,9 @@
 package nl.knaw.huygens.alexandria.endpoint.resource;
 
-import io.swagger.annotations.Api;
-
 import javax.inject.Inject;
 import javax.ws.rs.PathParam;
 
+import io.swagger.annotations.Api;
 import nl.knaw.huygens.alexandria.endpoint.AccountableProvenanceEndpoint;
 import nl.knaw.huygens.alexandria.endpoint.LocationBuilder;
 import nl.knaw.huygens.alexandria.endpoint.UUIDParam;
@@ -14,14 +13,14 @@ import nl.knaw.huygens.alexandria.service.AlexandriaService;
 @Api("resources")
 public class ResourceProvenanceEndpoint extends AccountableProvenanceEndpoint {
   @Inject
-  public ResourceProvenanceEndpoint(AlexandriaService service, //
+  public ResourceProvenanceEndpoint(AlexandriaService service,  //
       @PathParam("uuid") final UUIDParam uuidParam, LocationBuilder locationBuilder) {
     super(service, uuidParam, locationBuilder);
   }
 
   @Override
   protected Accountable getAccountable() {
-    return service.readResource(uuid).get();
+    return service.readResource(uuid).orElseThrow(ResourcesEndpoint.resourceNotFoundForId(uuid));
   };
 
 }
