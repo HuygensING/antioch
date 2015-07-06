cdc985fbaa5304bf61e654a3f931f125cd3d4ad8
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-07-06 13:10:33 +0200

    show time offset in date/time format

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/AboutEndpoint.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/AboutEndpoint.java
@@ -5,7 +5,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 import java.util.PropertyResourceBundle;
-import java.util.TimeZone;
 
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
@@ -34,8 +33,7 @@ public class AboutEndpoint extends JSONEndpoint {
     Map<String, String> data = Maps.newLinkedHashMap();
     data.put("version", getProperty("version"));
     data.put("builddate", getProperty("builddate"));
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-    simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT-1.00"));
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
     data.put("started", simpleDateFormat.format(this.starttime));
     return Response.ok(data).build();
   }
