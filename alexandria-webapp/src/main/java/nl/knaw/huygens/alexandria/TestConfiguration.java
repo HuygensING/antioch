025b5dc28af8bc6cd52b7865ee0918c6fbf55a64
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-07-08 13:27:36 +0200

    use tc23

--- alexandria-webapp/src/main/java/nl/knaw/huygens/alexandria/TestConfiguration.java
+++ alexandria-webapp/src/main/java/nl/knaw/huygens/alexandria/TestConfiguration.java
@@ -2,16 +2,12 @@ package nl.knaw.huygens.alexandria;
 
 import java.net.URI;
 
-import javax.ws.rs.core.UriBuilder;
-
-import nl.knaw.huygens.alexandria.config.AlexandriaConfiguration;
-
 public class TestConfiguration implements AlexandriaConfiguration {
 
   @Override
   public URI getBaseURI() {
     // TODO: hide url
-    return UriBuilder.fromUri("http://alexandria.huygens.knaw.nl/test-alexandria/").build();
+    return UriBuilder.fromUri("http://tc23.huygens.knaw.nl/test-alexandria/").build();
   }
 
   @Override
