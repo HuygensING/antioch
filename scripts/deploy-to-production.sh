4a3e371b9fd43316ff22a23cd22f896af6cef279
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-07-08 15:06:59 +0200

    create deploy-to-production script

---
+++ scripts/deploy-to-production.sh
@@ -0,0 +1,12 @@
+function divider {
+  echo
+  echo "--------------------------------------------------------------------------------"
+  echo
+}
+
+base=http://alexandria.huygens.knaw.nl/alexandria
+mvn package tomcat7:redeploy -P prod -pl alexandria-webapp -am
+divider
+sleep 20 # wait for the server to boot
+curlCmd="curl -sSf $base/about"
+echo $curlCmd
