6e73ceb18dd0bca4b9885e2746690e04cfaf48a5
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-07-10 09:44:05 +0200

    Refactor and confine unchecked stuff to separate method

--- alexandria-main/src/test/java/nl/knaw/huygens/alexandria/endpoint/about/AboutEndpointTest.java
+++ alexandria-main/src/test/java/nl/knaw/huygens/alexandria/endpoint/about/AboutEndpointTest.java
@@ -1,17 +1,14 @@
 package nl.knaw.huygens.alexandria.endpoint.about;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.StrictAssertions.assertThat;
-
-import java.util.Map;
 
 import javax.ws.rs.core.Response;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.util.Map;
 
 import nl.knaw.huygens.Log;
 import nl.knaw.huygens.alexandria.endpoint.MockedServiceEndpointTest;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class AboutEndpointTest extends MockedServiceEndpointTest {
   @BeforeClass
@@ -24,9 +21,13 @@ public class AboutEndpointTest extends MockedServiceEndpointTest {
     Response response = target("about").request().get();
     Log.info("response={}", response);
     assertThat(response.getStatus()).isEqualTo(200);
-    @SuppressWarnings("unchecked")
-    Map<String, Object> map = response.readEntity(Map.class);
-    Log.info("result={}", map);
-    assertThat(map).containsKeys("version", "builddate", "started");
+    assertThat(responseEntityAsMap(response)).containsKeys("version", "buildDate", "commitId", "startedAt");
+  }
+
+  @SuppressWarnings("unchecked")
+  private Map<String, Object> responseEntityAsMap(Response response) {
+    final Map result = response.readEntity(Map.class);
+    Log.info("result={}", result);
+    return result;
   }
 }
