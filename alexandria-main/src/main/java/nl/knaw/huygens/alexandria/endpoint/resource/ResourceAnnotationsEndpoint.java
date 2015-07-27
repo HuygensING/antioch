02c1a9e4060a2db6fe7fa055965953579682ea81
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-07-27 10:05:57 +0200

    NLA-68
    on /resources/{id}/annotations, also return the annotations for the
    subresources

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourceAnnotationsEndpoint.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourceAnnotationsEndpoint.java
@@ -1,14 +1,28 @@
 package nl.knaw.huygens.alexandria.endpoint.resource;
 
+import static java.util.stream.Collectors.toSet;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import nl.knaw.huygens.alexandria.endpoint.AnnotatableObjectAnnotationsEndpoint;
 import nl.knaw.huygens.alexandria.endpoint.AnnotationCreationRequestBuilder;
 import nl.knaw.huygens.alexandria.endpoint.LocationBuilder;
 import nl.knaw.huygens.alexandria.endpoint.UUIDParam;
+import nl.knaw.huygens.alexandria.endpoint.annotation.AnnotationEntity;
 import nl.knaw.huygens.alexandria.model.AbstractAnnotatable;
+import nl.knaw.huygens.alexandria.model.AlexandriaAnnotation;
 import nl.knaw.huygens.alexandria.model.AlexandriaResource;
 import nl.knaw.huygens.alexandria.service.AlexandriaService;
 
@@ -17,9 +31,9 @@ public class ResourceAnnotationsEndpoint extends AnnotatableObjectAnnotationsEnd
 
   // TODO: how to remove this duplicated inject/constructor?
   @Inject
-  public ResourceAnnotationsEndpoint(AlexandriaService service,  //
-      AnnotationCreationRequestBuilder requestBuilder,  //
-      LocationBuilder locationBuilder,  //
+  public ResourceAnnotationsEndpoint(AlexandriaService service, //
+      AnnotationCreationRequestBuilder requestBuilder, //
+      LocationBuilder locationBuilder, //
       @PathParam("uuid") final UUIDParam uuidParam) {
     super(service, requestBuilder, locationBuilder, uuidParam);
   }
@@ -37,6 +51,29 @@ public class ResourceAnnotationsEndpoint extends AnnotatableObjectAnnotationsEnd
   @Override
   protected AnnotationCreationRequestBuilder getAnnotationCreationRequestBuilder() {
     return requestBuilder.ofResource(uuid);
+  }
+
+  @GET
+  @ApiOperation(value = "get the annotations of the resource and its immediate subresources", response = AnnotationEntity.class)
+  @Override
+  public Response get() {
+    Stream<AlexandriaAnnotation> resourceAnnotationsStream = getAnnotatableObject().getAnnotations().stream();
+
+    Stream<AlexandriaAnnotation> subresourceAnnotationsStream = ((AlexandriaResource) getAnnotatableObject())//
+        .getSubResourcePointers().stream()//
+        .map(service::dereference)//
+        .filter(Optional::isPresent)//
+        .map(Optional::get)//
+        .flatMap(a -> ((AlexandriaResource) a).getAnnotations().stream());
+
+    Stream<AlexandriaAnnotation> joinedStream = Stream.concat(resourceAnnotationsStream, subresourceAnnotationsStream);
+
+    final Set<AnnotationEntity> annotationEntities = joinedStream//
+        .map((AlexandriaAnnotation a) -> AnnotationEntity.of(a).withLocationBuilder(locationBuilder))//
+        .collect(toSet());
+
+    Map<String, Set<AnnotationEntity>> entity = ImmutableMap.of("annotations", annotationEntities);
+    return Response.ok(entity).build();
   }
 
 }
