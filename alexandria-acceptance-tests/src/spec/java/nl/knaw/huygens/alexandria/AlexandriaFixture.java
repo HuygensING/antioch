745f2b46011155748bb29336e6d469293cca6b3e
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-08-12 14:50:48 +0200

    Unmark Alexandria test as ExpectedToFail (apparently not failing if we mark its failing subtest as ExpectedToFail

--- alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/AlexandriaFixture.java
+++ alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/AlexandriaFixture.java
@@ -1,11 +1,9 @@
 package nl.knaw.huygens.alexandria;
 
 import nl.knaw.huygens.alexandria.concordion.AlexandriaAcceptanceTest;
-import org.concordion.api.ExpectedToFail;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-@ExpectedToFail
 @RunWith(ConcordionRunner.class)
 public class AlexandriaFixture extends AlexandriaAcceptanceTest {
 }
