c5010c9aa01e005887624b618ef2f2906a30a4da
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-08-27 14:18:35 +0200

    add index on "uuid" property to tinkergraph instance

--- alexandria-service/src/main/java/nl/knaw/huygens/alexandria/service/TinkerGraphService.java
+++ alexandria-service/src/main/java/nl/knaw/huygens/alexandria/service/TinkerGraphService.java
@@ -3,6 +3,7 @@ package nl.knaw.huygens.alexandria.service;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 import nl.knaw.huygens.alexandria.endpoint.LocationBuilder;
@@ -11,7 +12,13 @@ import nl.knaw.huygens.alexandria.storage.Storage;
 @Singleton
 public class TinkerGraphService extends TinkerPopService {
 
-  protected static final Storage STORAGE = new Storage(TinkerGraph.open());
+  protected static final Storage STORAGE = getStorage();
+
+  private static Storage getStorage() {
+    TinkerGraph tg = TinkerGraph.open();
+    tg.createIndex("uuid", Vertex.class);
+    return new Storage(tg);
+  }
 
   @Inject
   public TinkerGraphService(LocationBuilder locationBuilder) {
