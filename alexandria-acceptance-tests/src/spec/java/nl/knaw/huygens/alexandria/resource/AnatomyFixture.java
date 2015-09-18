4d089f969691b30d1f07e33af8c7b2e231cab02a
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-09-18 12:08:32 +0200

    Add test + docs on "Anatomy of a Resource object"

--- alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/resource/AnatomyFixture.java
+++ alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/resource/AnatomyFixture.java
@@ -1,12 +1,16 @@
 package nl.knaw.huygens.alexandria.resource;
 
+import static java.util.UUID.fromString;
+import static java.util.UUID.randomUUID;
+
 import java.time.Instant;
 import java.util.UUID;
 
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-import nl.knaw.huygens.Log;
+import nl.knaw.huygens.alexandria.model.AlexandriaAnnotationBody;
+import nl.knaw.huygens.alexandria.model.AlexandriaResource;
 import nl.knaw.huygens.alexandria.model.AlexandriaState;
 import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
 
@@ -14,8 +18,37 @@ import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
 public class AnatomyFixture extends ResourcesBase {
 
   public void resourceExists(String id) {
-    Log.trace("resourceExists: id=[{}]", id);
-    service().createOrUpdateResource(UUID.fromString(id), aRef(), aProvenance(), confirmed());
+    service().createOrUpdateResource(fromString(id), aRef(), aProvenance(), confirmed());
+  }
+
+  public String hasSubresource(String id) {
+    return service().createSubResource(randomUUID(), fromString(id), aSub(), aProvenance(), confirmed()) //
+        .getId().toString();
+  }
+
+  public String hasAnnotation(String id) {
+    final UUID uuid = fromString(id);
+    return service().annotate(theResource(uuid), anAnnotationBody(uuid), aProvenance()).getId().toString();
+  }
+
+  private AlexandriaResource theResource(UUID resId) {
+    return service().readResource(resId).get();
+  }
+
+  private AlexandriaAnnotationBody anAnnotationBody(UUID resId) {
+    return service().createAnnotationBody(resId, aType(), aValue(), aProvenance(), confirmed());
+  }
+
+  private String aSub() {
+    return "/some/folia/expression";
+  }
+
+  private String aType() {
+    return "type";
+  }
+
+  private String aValue() {
+    return "value";
   }
 
   private String aRef() {
