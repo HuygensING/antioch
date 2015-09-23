22445656116d728b19213182dfa610cb638418d9
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-09-23 08:30:04 +0200

    Reduce amount of logging

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/config/ValidationConfigurationContextResolver.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/config/ValidationConfigurationContextResolver.java
@@ -4,24 +4,19 @@ import javax.inject.Inject;
 import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.ext.ContextResolver;
 
-import nl.knaw.huygens.Log;
-
 import org.glassfish.jersey.server.validation.ValidationConfig;
 import org.glassfish.jersey.server.validation.internal.InjectingConstraintValidatorFactory;
 
 public class ValidationConfigurationContextResolver implements ContextResolver<ValidationConfig> {
-
   private final ResourceContext resourceContext;
 
   @Inject
   public ValidationConfigurationContextResolver(ResourceContext resourceContext) {
-    Log.trace("resourceContext=[{}]", resourceContext);
     this.resourceContext = resourceContext;
   }
 
   @Override
   public ValidationConfig getContext(Class<?> type) {
-    Log.trace("getContext: type=[{}]", type);
     return new ValidationConfig() //
         // .messageInterpolator(new ResourceBundleMessageInterpolator(//
         // new PlatformResourceBundleLocator("ValidationMessages")))
