96c9afe0317292dc16b01f39e573a4e8456d2aa9
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2015-10-13 10:42:15 +0200

    Suppress maven+intellij warning about unchecked cast Fix various CRLF issues

--- alexandria-service-tinkerpop-titan/src/main/java/nl/knaw/huygens/alexandria/service/TitanService.java
+++ alexandria-service-tinkerpop-titan/src/main/java/nl/knaw/huygens/alexandria/service/TitanService.java
@@ -17,12 +17,12 @@ import java.util.stream.StreamSupport;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -128,7 +128,7 @@ public class TitanService extends TinkerPopService {
   public Map<String, Object> getMetadata() {
     TitanManagement mgmt = titanGraph.openManagement();
     Map<String, Object> metadata = super.getMetadata();
-    Map<String, Object> storageMap = (Map<String, Object>) metadata.get("storage");
+    @SuppressWarnings("unchecked") Map<String, Object> storageMap = (Map<String, Object>) metadata.get("storage");
     storageMap.put("vertexIndexes", indexInfo(mgmt, Vertex.class));
     storageMap.put("edgeIndexes", indexInfo(mgmt, Edge.class));
     return metadata;
