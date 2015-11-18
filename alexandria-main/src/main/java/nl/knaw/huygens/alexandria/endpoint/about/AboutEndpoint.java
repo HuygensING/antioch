97e67045335fd25a9d71cbc8d9730e858966d9d9
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-11-18 11:27:10 +0100

    refactoring: extract PropertiesConfig

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/about/AboutEndpoint.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/about/AboutEndpoint.java
@@ -1,9 +1,34 @@
 package nl.knaw.huygens.alexandria.endpoint.about;
 
+/*
+ * #%L
+ * alexandria-main
+ * =======
+ * Copyright (C) 2015 Huygens ING (KNAW)
+ * =======
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import java.io.IOException;
 import java.net.URI;
 import java.time.Instant;
 import java.time.temporal.TemporalAmount;
 import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.Optional;
 import java.util.PropertyResourceBundle;
 
 import javax.inject.Inject;
@@ -16,9 +41,10 @@ import com.google.common.collect.Maps;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import nl.knaw.huygens.Log;
 import nl.knaw.huygens.alexandria.config.AlexandriaConfiguration;
-import nl.knaw.huygens.alexandria.config.PropertiesConfiguration;
 import nl.knaw.huygens.alexandria.endpoint.JSONEndpoint;
+import nl.knaw.huygens.alexandria.jaxrs.Annotations.AuthorizationRequired;
 import nl.knaw.huygens.alexandria.jersey.AlexandriaApplication;
 import nl.knaw.huygens.alexandria.service.AlexandriaService;
 
@@ -26,20 +52,19 @@ import nl.knaw.huygens.alexandria.service.AlexandriaService;
 @Path("about")
 @Api("about")
 public class AboutEndpoint extends JSONEndpoint {
+  private static final String PROPERTIES_FILE = "about.properties";
   private static final String STARTED_AT = System.getProperty(AlexandriaApplication.STARTTIME_PROPERTY, Instant.now().toString());
-  private static PropertyResourceBundle propertyResourceBundle;
-
   private final TemporalAmount tentativesTTL;
   private final URI baseURI;
   private final AlexandriaService service;
-  private PropertiesConfiguration properties;
+  private final PropertiesConfig properties;
 
   @Inject
-  public AboutEndpoint(AlexandriaConfiguration config, PropertiesConfiguration pConfig, AlexandriaService service) {
+  public AboutEndpoint(AlexandriaConfiguration config, AlexandriaService service) {
     this.service = service;
     this.baseURI = config.getBaseURI();
     this.tentativesTTL = service.getTentativesTimeToLive();
-    this.properties = pConfig;
+    this.properties = new PropertiesConfig(PROPERTIES_FILE);
   }
 
   /**
@@ -52,12 +77,12 @@ public class AboutEndpoint extends JSONEndpoint {
   public Response getMetadata() {
     final Map<String, String> data = Maps.newLinkedHashMap();
     data.put("baseURI", baseURI.toString());
-    data.put("buildDate", properties.getProperty("buildDate"));
-    data.put("commitId", properties.getProperty("commitId"));
-    data.put("scmBranch", properties.getProperty("scmBranch"));
+    data.put("buildDate", properties.getProperty("buildDate").get());
+    data.put("commitId", properties.getProperty("commitId").get());
+    data.put("scmBranch", properties.getProperty("scmBranch").get());
     data.put("startedAt", STARTED_AT);
     data.put("tentativesTTL", tentativesTTL.toString());
-    data.put("version", properties.getProperty("version"));
+    data.put("version", properties.getProperty("version").get());
     return Response.ok(data).build();
   }
 
@@ -66,11 +91,39 @@ public class AboutEndpoint extends JSONEndpoint {
    *
    * @return about-data map
    */
+  @AuthorizationRequired
   @GET
   @Path("service")
   @ApiOperation("get information about the service")
   public Response getGraphMetadata() {
     return Response.ok(service.getMetadata()).build();
+  }
+
+  public class PropertiesConfig {
+    private PropertyResourceBundle propertyResourceBundle;
+
+    public PropertiesConfig(String propertiesFile) {
+      try {
+        propertyResourceBundle = new PropertyResourceBundle(//
+            Thread.currentThread().getContextClassLoader().getResourceAsStream(propertiesFile));
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException("Couldn't read " + propertiesFile);
+      }
+    }
+
+    public synchronized Optional<String> getProperty(String key) {
+      try {
+        return Optional.ofNullable(propertyResourceBundle.getString(key));
+      } catch (MissingResourceException e) {
+        Log.warn("Missing expected resource: [{}] -- winging it", key);
+        return Optional.empty();
+      } catch (ClassCastException e) {
+        Log.warn("Property value for key [{}] cannot be transformed to String", key);
+        return Optional.empty();
+      }
+    }
+
   }
 
 }
