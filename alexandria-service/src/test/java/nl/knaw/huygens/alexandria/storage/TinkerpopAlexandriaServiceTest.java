cb6683fdcd7b168a2ac8bac8c5fb430d1788ff5e
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2016-03-18 08:33:55 +0100

    Commented compiler complaints on unchecked / unsafe call
    @Suppresswarnings("unchecked") not allowed at this point (static method block) and I didn't have time to look into it more.

--- alexandria-service/src/test/java/nl/knaw/huygens/alexandria/storage/TinkerpopAlexandriaServiceTest.java
+++ alexandria-service/src/test/java/nl/knaw/huygens/alexandria/storage/TinkerpopAlexandriaServiceTest.java
@@ -57,6 +57,14 @@ public class TinkerpopAlexandriaServiceTest {
   private final TinkerPopService service = new TinkerPopService(mockStorage, new LocationBuilder(new MockConfiguration(), new EndpointPathResolver()), new InMemoryTextService());
 
   static {
+    /* Maven compiler plugin warns about unchecked / unsafe code here:
+[WARNING] TinkerpopAlexandriaServiceTest.java:[68,50] unchecked conversion
+  required: java.util.function.Supplier<T>
+  found:    java.util.function.Supplier
+[WARNING] TinkerpopAlexandriaServiceTest.java:[68,9] unchecked method invocation: method when in class org.mockito.Mockito is applied to given types
+  required: T
+  found: java.lang.Object
+     */
     when(mockStorage.runInTransaction(Mockito.any(Supplier.class))).thenCallRealMethod();//
   }
 
