404767e8b870bf84e02640cc5438c92d8f24845c
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-03-23 15:12:49 +0100

    Merge branch 'develop' into test-async-endpoint
    # Conflicts:
    #	alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/resource/ResourceTextEndpoint.java
    #	alexandria-service/src/main/java/nl/knaw/huygens/alexandria/storage/Storage.java
    #	pom.xml

--- alexandria-service/src/test/java/nl/knaw/huygens/alexandria/storage/TinkerpopAlexandriaServiceTest.java
+++ alexandria-service/src/test/java/nl/knaw/huygens/alexandria/storage/TinkerpopAlexandriaServiceTest.java
@@ -22,36 +22,28 @@ package nl.knaw.huygens.alexandria.storage;
  * #L%
  */
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.time.Instant;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.function.Supplier;
-
+import nl.knaw.huygens.Log;
+import nl.knaw.huygens.alexandria.api.model.AlexandriaState;
+import nl.knaw.huygens.alexandria.config.MockConfiguration;
+import nl.knaw.huygens.alexandria.endpoint.EndpointPathResolver;
+import nl.knaw.huygens.alexandria.endpoint.LocationBuilder;
+import nl.knaw.huygens.alexandria.model.*;
+import nl.knaw.huygens.alexandria.service.TinkerPopService;
+import nl.knaw.huygens.alexandria.storage.frames.AnnotationVF;
+import nl.knaw.huygens.alexandria.text.InMemoryTextService;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import nl.knaw.huygens.Log;
-import nl.knaw.huygens.alexandria.api.model.AlexandriaState;
-import nl.knaw.huygens.alexandria.config.MockConfiguration;
-import nl.knaw.huygens.alexandria.endpoint.EndpointPathResolver;
-import nl.knaw.huygens.alexandria.endpoint.LocationBuilder;
-import nl.knaw.huygens.alexandria.model.Accountable;
-import nl.knaw.huygens.alexandria.model.AlexandriaAnnotation;
-import nl.knaw.huygens.alexandria.model.AlexandriaAnnotationBody;
-import nl.knaw.huygens.alexandria.model.AlexandriaResource;
-import nl.knaw.huygens.alexandria.model.IdentifiablePointer;
-import nl.knaw.huygens.alexandria.model.TentativeAlexandriaProvenance;
-import nl.knaw.huygens.alexandria.service.TinkerPopService;
-import nl.knaw.huygens.alexandria.storage.frames.AnnotationVF;
-import nl.knaw.huygens.alexandria.text.InMemoryTextService;
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class TinkerpopAlexandriaServiceTest {
   private static Storage mockStorage = mock(Storage.class);
