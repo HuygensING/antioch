3a9a9b5f77ac6497f8f253ce883732fe6a3421bf
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-04-22 18:19:16 +0200

    [NLA-248] Some improvements to acceptance test for AddUniqueIdCommand

--- alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/resource/ResourcesBase.java
+++ alexandria-acceptance-tests/src/spec/java/nl/knaw/huygens/alexandria/resource/ResourcesBase.java
@@ -10,12 +10,12 @@ package nl.knaw.huygens.alexandria.resource;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -25,6 +25,7 @@ package nl.knaw.huygens.alexandria.resource;
 import org.junit.BeforeClass;
 
 import nl.knaw.huygens.alexandria.concordion.AlexandriaAcceptanceTest;
+import nl.knaw.huygens.alexandria.endpoint.command.CommandsEndpoint;
 import nl.knaw.huygens.alexandria.endpoint.resource.ResourcesEndpoint;
 import nl.knaw.huygens.alexandria.jersey.exceptionmappers.WebApplicationExceptionMapper;
 
@@ -32,6 +33,7 @@ public class ResourcesBase extends AlexandriaAcceptanceTest {
   @BeforeClass
   public static void registerEndpoint() {
     register(ResourcesEndpoint.class);
+    register(CommandsEndpoint.class);
     register(WebApplicationExceptionMapper.class);
   }
 }
