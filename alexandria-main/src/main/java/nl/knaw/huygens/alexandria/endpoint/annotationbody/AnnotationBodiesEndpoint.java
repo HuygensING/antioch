06f2e32a06ff7d2e4198250d8ccfee7f52be4bfe
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-05-09 09:48:08 +0200

    Extract conflict() to JSONEndpoint

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/annotationbody/AnnotationBodiesEndpoint.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/annotationbody/AnnotationBodiesEndpoint.java
@@ -12,7 +12,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 /*
  * #%L
@@ -81,7 +80,7 @@ public class AnnotationBodiesEndpoint extends JSONEndpoint {
       return created(locationOf(prototype.getId().getValue()));
     }
 
-    return Response.status(Status.CONFLICT).build();
+    return conflict();
   }
 
   @DELETE
