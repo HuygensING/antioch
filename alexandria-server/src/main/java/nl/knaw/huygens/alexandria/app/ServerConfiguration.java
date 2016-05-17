fcc7fcc8f61eb3f5f910ecc52dab21a8557e30f9
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-05-17 17:06:38 +0200

    [NLA-188] Use properties file in ~/.alexandria

--- alexandria-server/src/main/java/nl/knaw/huygens/alexandria/app/ServerConfiguration.java
+++ alexandria-server/src/main/java/nl/knaw/huygens/alexandria/app/ServerConfiguration.java
@@ -1,8 +1,11 @@
 package nl.knaw.huygens.alexandria.app;
 
 import java.io.File;
-import java.net.NetworkInterface;
-import java.net.SocketException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /*
  * #%L
@@ -29,36 +32,110 @@ import java.net.SocketException;
 import java.net.URI;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
 
-import javax.ws.rs.core.UriBuilder;
+import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import nl.knaw.huygens.Log;
 import nl.knaw.huygens.alexandria.config.AbstractAlexandriaConfigurationUsingAlexandriaProperties;
 
 public class ServerConfiguration extends AbstractAlexandriaConfigurationUsingAlexandriaProperties {
+  private static final File ALEXANDRIA_ROOT = new File(System.getProperty("user.home"), ".alexandria");
+  private static final File ALEXANDRIA_PROPERTIES_FILE = new File(ALEXANDRIA_ROOT, "alexandria.properties");
+
+  private static final String PROPERTY_LOG_LEVEL = "logLevel";
+  private static final String PROPERTY_ADMIN_KEY = "adminKey";
+  private static final String PROPERTY_STORAGE_DIRECTORY = "storageDirectory";
+  private static final String PROPERTY_BASE_URI = "baseURI";
+
+  private static final String DEFAULT_BASE_URI = "http://localhost:2015/";
+  private static final String DEFAULT_STORAGE_DIRECTORY = new File(ALEXANDRIA_ROOT, "data").getAbsolutePath();
+  private static final String DEFAULT_ADMIN_KEY = "admin";
+
+  private String baseURI = DEFAULT_BASE_URI;
+  private String storageDirectory = DEFAULT_STORAGE_DIRECTORY;
+  private String adminKey = DEFAULT_ADMIN_KEY;
+  private Level logLevel = Level.WARN;
+
+  public ServerConfiguration() {
+    if (ALEXANDRIA_PROPERTIES_FILE.exists()) {
+      initFromFile();
+    } else {
+      initFromCommandLine();
+    }
+    setLogLevel();
+  }
 
   @Override
   public URI getBaseURI() {
-    return URI.create("http://localhost:2015/");
+    return URI.create(baseURI);
   }
 
   @Override
   public String getStorageDirectory() {
-    return new File(System.getProperty("user.dir", "."), "alexandria-data").getAbsolutePath();
+    return storageDirectory;
   }
 
   @Override
   public Map<String, String> getAuthKeyIndex() {
-    return Collections.singletonMap("admin", "admin");
+    return Collections.singletonMap(DEFAULT_ADMIN_KEY, DEFAULT_ADMIN_KEY);
   }
 
   @Override
   public String getAdminKey() {
-    return "admin";
+    return adminKey;
   }
 
   @Override
   public Boolean asynchronousEndpointsAllowed() {
     return true;
+  }
+
+  private void initFromFile() {
+    Properties properties = new Properties();
+    try (InputStream in = new FileInputStream(ALEXANDRIA_PROPERTIES_FILE)) {
+      properties.load(in);
+      baseURI = properties.getProperty(PROPERTY_BASE_URI);
+      storageDirectory = properties.getProperty(PROPERTY_STORAGE_DIRECTORY);
+      adminKey = properties.getProperty(PROPERTY_ADMIN_KEY);
+      logLevel = Level.toLevel(properties.getProperty(PROPERTY_LOG_LEVEL));
+    } catch (IOException e) {
+      e.printStackTrace();
+      Log.error("Error reading properties from {}: {}", ALEXANDRIA_PROPERTIES_FILE.getAbsolutePath(), e.getMessage());
+    }
+
+  }
+
+  private void initFromCommandLine() {
+    try (Scanner keyboard = new Scanner(System.in)) {
+      // System.out.println("base uri: (default: " + DEFAULT_BASE_URI + "):");
+      // baseURI = StringUtils.defaultIfBlank(keyboard.nextLine(), DEFAULT_BASE_URI);
+
+      Properties properties = new Properties();
+      if (!ALEXANDRIA_ROOT.exists()) {
+        if (!ALEXANDRIA_ROOT.mkdir()) {
+          throw new RuntimeException("Fatal error: couldn't create directory " + ALEXANDRIA_ROOT.getAbsolutePath());
+        }
+      }
+      try (OutputStream out = new FileOutputStream(ALEXANDRIA_PROPERTIES_FILE)) {
+        properties.setProperty(PROPERTY_BASE_URI, baseURI);
+        properties.setProperty(PROPERTY_STORAGE_DIRECTORY, storageDirectory);
+        properties.setProperty(PROPERTY_ADMIN_KEY, adminKey);
+        properties.setProperty(PROPERTY_LOG_LEVEL, logLevel.toString());
+        properties.store(out, "Alexandria settings");
+      } catch (IOException e) {
+        e.printStackTrace();
+        Log.error("Error writing properties to {}: {}", ALEXANDRIA_PROPERTIES_FILE.getAbsolutePath(), e.getMessage());
+      }
+    }
+  }
+
+  private void setLogLevel() {
+    Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+    rootLogger.setLevel(logLevel);
   }
 
 }
