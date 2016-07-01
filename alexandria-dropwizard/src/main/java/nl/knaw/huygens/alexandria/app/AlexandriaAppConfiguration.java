66d9664a9f1215ff267d476b08c744aad143c5d5
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-07-01 14:47:20 +0200

    Adds a dropwizard-based alexandria server using just about and home endpoints

--- alexandria-dropwizard/src/main/java/nl/knaw/huygens/alexandria/app/AlexandriaAppConfiguration.java
+++ alexandria-dropwizard/src/main/java/nl/knaw/huygens/alexandria/app/AlexandriaAppConfiguration.java
@@ -1,22 +1,84 @@
 package nl.knaw.huygens.alexandria.app;
 
+import java.net.URI;
+import java.util.Map;
+
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableMap;
 
 import io.dropwizard.Configuration;
+import nl.knaw.huygens.Log;
+import nl.knaw.huygens.alexandria.config.AlexandriaConfiguration;
 
-public class AlexandriaAppConfiguration extends Configuration {
+public class AlexandriaAppConfiguration extends Configuration implements AlexandriaConfiguration {
+  @NotNull
+  private URI baseURI;
   @NotEmpty
-  private String name;
+  private String adminKey;
+  @NotEmpty
+  private String storageDirectory;
+  private Boolean asynchronousEndpointsAllowed = true;
 
-  @JsonProperty
-  String getName() {
-    return name;
+  public AlexandriaAppConfiguration() {
+    super();
+    Log.info("AlexandriaAppConfiguration initialized");
   }
 
   @JsonProperty
-  public void setName(String name) {
-    this.name = name;
+  public void setBaseURI(URI baseURI) {
+    this.baseURI = baseURI;
   }
+
+  @Override
+  @JsonProperty
+  public URI getBaseURI() {
+    return baseURI;
+  }
+
+  @JsonProperty
+  public void setStorageDirectory(String storageDirectory) {
+    this.storageDirectory = storageDirectory;
+  }
+
+  @Override
+  @JsonProperty
+  public String getStorageDirectory() {
+    return storageDirectory;
+  }
+
+  @Override
+  public Map<String, String> getAuthKeyIndex() {
+    return ImmutableMap.of("admin", adminKey);
+  }
+
+  @JsonProperty
+  public void setAdminKey(String adminKey) {
+    this.adminKey = adminKey;
+  }
+
+  @Override
+  @JsonProperty
+  public String getAdminKey() {
+    return adminKey;
+  }
+
+  @Override
+  public Boolean asynchronousEndpointsAllowed() {
+    return getAsynchronousEndpointsAllowed();
+  }
+
+  @JsonProperty
+  public void setAsynchronousEndpointsAllowed(Boolean asynchronousEndpointsAllowed) {
+    this.asynchronousEndpointsAllowed = asynchronousEndpointsAllowed;
+  }
+
+  @JsonProperty
+  public Boolean getAsynchronousEndpointsAllowed() {
+    return asynchronousEndpointsAllowed;
+  }
+
 }
--- alexandria-dropwizard/src/main/java/nl/knaw/huygens/alexandria/app/AlexandriaApplication.java
+++ alexandria-dropwizard/src/main/java/nl/knaw/huygens/alexandria/app/AlexandriaApplication.java
@@ -1,14 +1,22 @@
 package nl.knaw.huygens.alexandria.app;
 
-import java.util.Properties;
+import com.google.inject.Binder;
+import com.google.inject.Module;
 
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.java8.Java8Bundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import nl.knaw.huygens.Log;
+import nl.knaw.huygens.alexandria.config.AlexandriaConfiguration;
+import nl.knaw.huygens.alexandria.endpoint.EndpointPathResolver;
+import nl.knaw.huygens.alexandria.endpoint.JSONEndpoint;
+import nl.knaw.huygens.alexandria.endpoint.LocationBuilder;
+import nl.knaw.huygens.alexandria.endpoint.about.AboutEndpoint;
+import nl.knaw.huygens.alexandria.endpoint.homepage.HomePageEndpoint;
 import nl.knaw.huygens.alexandria.health.AboutHealthCheck;
+import nl.knaw.huygens.alexandria.service.AlexandriaService;
+import nl.knaw.huygens.alexandria.service.TitanService;
 
 public class AlexandriaApplication extends Application<AlexandriaAppConfiguration> {
   public static void main(String... args) throws Exception {
@@ -24,8 +32,10 @@ public class AlexandriaApplication extends Application<AlexandriaAppConfiguratio
   public void initialize(Bootstrap<AlexandriaAppConfiguration> bootstrap) {
     bootstrap.addBundle(new AssetsBundle());
     bootstrap.addBundle(new Java8Bundle());
+    // AlexandriaServletModule alexandriaServletModule = new AlexandriaServletModule();
     // GuiceBundle<AlexandriaAppConfiguration> guiceBundle = GuiceBundle.<AlexandriaAppConfiguration> newBuilder()//
-    // .addModule(new AlexandriaServletModule())//
+    // .addModule(new ConfigModule())//
+    // .addModule(alexandriaServletModule)//
     // .enableAutoConfig(getClass().getPackage().getName())//
     // .setConfigClass(AlexandriaAppConfiguration.class)//
     // .build();
@@ -34,18 +44,30 @@ public class AlexandriaApplication extends Application<AlexandriaAppConfiguratio
   }
 
   @Override
-  public void run(AlexandriaAppConfiguration alexandriaConfiguration, Environment environment) throws Exception {
-    Properties gitProperties = new Properties();
-    gitProperties.load(getClass().getClassLoader().getResourceAsStream("git.properties"));
-    final String version = gitProperties.getProperty("git.commit.id");
-    Log.info("Launching Alexandria version: {}", version);
-
-    Log.trace("config.name=[{}]", alexandriaConfiguration.getName());
+  public void run(AlexandriaAppConfiguration config, Environment environment) throws Exception {
+    // Properties gitProperties = new Properties();
+    // gitProperties.load(getClass().getClassLoader().getResourceAsStream("git.properties"));
+    // final String version = gitProperties.getProperty("git.commit.id");
+    // Log.info("Launching Alexandria version: {}", version);
 
     environment.healthChecks().register("about", new AboutHealthCheck());
 
-    // environment.jersey().register(new AboutEndpoint());
+    LocationBuilder locationBuilder = new LocationBuilder(config, new EndpointPathResolver());
+    AlexandriaService service = new TitanService(locationBuilder, config);
+
+    environment.jersey().packages(JSONEndpoint.class.getPackage().getName());
+    environment.jersey().register(new AboutEndpoint(config, service));
+    environment.jersey().register(new HomePageEndpoint());
     // environment.jersey().register(new LoggingFilter());
     // environment.jersey().register(new JacksonJsonProvider());
+  }
+
+  static class ConfigModule implements Module {
+
+    @Override
+    public void configure(Binder binder) {
+      binder.bind(AlexandriaConfiguration.class).to(AlexandriaAppConfiguration.class);
+    }
+
   }
 }
