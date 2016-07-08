5ff6fd84fadda553f3085cffce431c2b541cd35d
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-07-08 14:53:31 +0200

    Makes Neo4JService singleton

--- alexandria-service-tinkerpop-neo4j/src/main/java/nl/knaw/huygens/alexandria/service/Neo4JService.java
+++ alexandria-service-tinkerpop-neo4j/src/main/java/nl/knaw/huygens/alexandria/service/Neo4JService.java
@@ -1,5 +1,7 @@
 package nl.knaw.huygens.alexandria.service;
 
+import java.io.File;
+
 /*
  * #%L
  * alexandria-service-tinkerpop-neo4j
@@ -10,12 +12,12 @@ package nl.knaw.huygens.alexandria.service;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -23,6 +25,7 @@ package nl.knaw.huygens.alexandria.service;
  */
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
 
@@ -31,6 +34,7 @@ import nl.knaw.huygens.alexandria.endpoint.LocationBuilder;
 import nl.knaw.huygens.alexandria.storage.Storage;
 import nl.knaw.huygens.alexandria.text.TextService;
 
+@Singleton
 public class Neo4JService extends TinkerPopService {
 
   @Inject
@@ -39,8 +43,17 @@ public class Neo4JService extends TinkerPopService {
   }
 
   private static Storage getStorage(AlexandriaConfiguration config) {
-    Neo4jGraph graph = Neo4jGraph.open(config.getStorageDirectory() + "/neo4jdb");
+    String dataDir = config.getStorageDirectory() + "/neo4jdb";
+    createWhenAbsent(dataDir);
+    Neo4jGraph graph = Neo4jGraph.open(dataDir);
     return new Storage(graph);
+  }
+
+  private static void createWhenAbsent(String dataDir) {
+    File file = new File(dataDir);
+    if (!file.isDirectory()) {
+      file.mkdirs();
+    }
   }
 
 }
