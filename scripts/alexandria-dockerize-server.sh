ff9df72a766c6b5c19d4387f48253e9e9774f3d4
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-09-01 12:05:02 +0200

    [NLA-187] Parameterizes & moves Dockerfile + adds dockerize script

---
+++ scripts/alexandria-dockerize-server.sh
@@ -0,0 +1,4 @@
+mvn package && \
+  export version=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='version']/text()" pom.xml) && \
+  cd alexandria-server && \
+    docker build -t huygensing/alexandria-server -t huygensing/alexandria-server:$version --build-arg version=$version .
