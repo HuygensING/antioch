8a1d7a749ee55c64db322b5d138af0fff3f47b43
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-01-16 17:40:38 +0100

    [NLA-341] Some code cleanup

--- alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/iiif/IIIFAnnotationListEndpoint.java
+++ alexandria-main/src/main/java/nl/knaw/huygens/alexandria/endpoint/iiif/IIIFAnnotationListEndpoint.java
@@ -15,18 +15,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
 
 import org.glassfish.jersey.server.ChunkedOutput;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.jsonldjava.utils.JsonUtils;
 import com.google.common.collect.Maps;
 
@@ -55,6 +48,7 @@ public class IIIFAnnotationListEndpoint extends AbstractIIIFEndpoint {
   }
 
   @POST
+  @Path("oldway")
   @Consumes(JSONLD_MEDIATYPE)
   public Response postAnnotationList(IIIFAnnotationList annotationList) {
     Map<String, Object> otherProperties = annotationList.getOtherProperties();
@@ -77,63 +71,21 @@ public class IIIFAnnotationListEndpoint extends AbstractIIIFEndpoint {
   }
 
   @POST
-  @Path("streaming")
-  @Consumes(JSONLD_MEDIATYPE)
-  public Response postAnnotationListStreaming(InputStream inputStream) throws JsonParseException, JsonMappingException, IOException {
-    StreamingOutput outStream = os -> {
-      JsonFactory jsonFactory = new JsonFactory();
-      JsonParser jParser = jsonFactory.createParser(inputStream);
-      JsonGenerator jGenerator = jsonFactory.createGenerator(os);
-      String context;
-      jGenerator.writeStartObject();
-      while (jParser.nextToken() != JsonToken.END_OBJECT) {
-        String fieldname = jParser.getCurrentName();
-        // Log.info("fieldname={}", fieldname);
-        if ("@context".equals(fieldname)) {
-          jParser.nextToken();
-          context = jParser.getText();
-          jGenerator.writeFieldName("@context");
-          jGenerator.writeString(context);
-
-        } else if ("resources".equals(fieldname)) {
-          parseResources(jParser, jGenerator);
-
-        } else if (fieldname != null) {
-          jGenerator.writeFieldName(fieldname);
-          jParser.nextToken();
-          jGenerator.writeString(jParser.getValueAsString());
-        }
-      }
-      jParser.close();
-
-      jGenerator.writeEndObject();
-      jGenerator.flush();
-      jGenerator.close();
-    };
-    return Response.ok(outStream).build();
-  }
-
-  @POST
-  @Path("chunked")
   @Consumes(JSONLD_MEDIATYPE)
   public ChunkedOutput<String> postAnnotationListChunked(InputStream inputStream) throws JsonParseException, JsonMappingException, IOException {
     final ChunkedOutput<String> output = new ChunkedOutput<>(String.class);
-
     AnnotationListHandler alh = new AnnotationListHandler(inputStream);
-
     new Thread() {
       @Override
       public void run() {
         try {
           String chunk;
           while ((chunk = alh.getNextString()) != null) {
-            System.out.println("> " + chunk);
             output.write(chunk);
           }
         } catch (IOException e) {
-          // IOException thrown when writing the
-          // chunks of response: should be handled
           e.printStackTrace();
+          throw new RuntimeException(e);
         } finally {
           try {
             output.close();
@@ -143,30 +95,7 @@ public class IIIFAnnotationListEndpoint extends AbstractIIIFEndpoint {
         }
       }
     }.start();
-
-    // the output will probably be returned even before
-    // a first chunk is written by the new thread
     return output;
-  }
-
-  private void parseResources(JsonParser jParser, JsonGenerator jGenerator) throws IOException {
-    jGenerator.writeFieldName("resources");
-    jParser.nextToken(); // "["
-    jGenerator.writeStartArray();
-    // parse each resource
-    ObjectMapper mapper = new ObjectMapper();
-    boolean first = true;
-    while (jParser.nextToken() != JsonToken.END_ARRAY) {
-      jGenerator.flush();
-      if (!first) {
-        jGenerator.writeRaw(",");
-      }
-      ObjectNode resourceNode = mapper.readTree(jParser);
-      // Log.info("resourceNode={}", resourceNode.get("resource").get(1).get("chars"));
-      jGenerator.writeRaw(resourceNode.toString());
-      first = false;
-    }
-    jGenerator.writeEndArray();
   }
 
   private Map<String, Object> dummySequence() {
