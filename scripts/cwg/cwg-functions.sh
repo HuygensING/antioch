7094e7604df05121305bc91762f1fea28856949c
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-08-02 11:48:21 +0200

    fix bash variables

--- scripts/cwg/cwg-functions.sh
+++ scripts/cwg/cwg-functions.sh
@@ -4,36 +4,36 @@
 function cwg-upload {
   a-log "create root resource for CWG project"
   ri=$(uuidgen)
-  cwg-create-root-resource $ri
-  root=$be/resources/$ri
+  cwg-create-root-resource ${ri}
+  root=${be}/resources/${ri}
 
   a-log "add baselayer definition to root resource"
-  curl -i -H "${authheader}" -X PUT $root/baselayerdefinition -H 'Content-type: application/json' --data @"cwg-bld.json"
-  curl $root/baselayerdefinition
+  curl -i -H "${authheader}" -X PUT ${root}/baselayerdefinition -H 'Content-type: application/json' --data @"cwg-bld.json"
+  curl ${root}/baselayerdefinition
 
   a-log "add Hugo Grotius resource"
-  cwg-create-subresource $root "groo001: Hugo Grotius corpus"
-  groo001=$suburi
+  cwg-create-subresource ${root} "groo001: Hugo Grotius corpus"
+  groo001=${suburi}
   a-log "add Constantijn Huygens resource"
-  cwg-create-subresource $root "huyg001: Constantijn Huygens corpus"
-  huyg001=$suburi
+  cwg-create-subresource ${root} "huyg001: Constantijn Huygens corpus"
+  huyg001=${suburi}
   a-log "add Christiaan Huygens resource"
-  cwg-create-subresource $root "huyg003: Christiaan Huygens corpus"
-  huyg003=$suburi
+  cwg-create-subresource ${root} "huyg003: Christiaan Huygens corpus"
+  huyg003=${suburi}
 
   a-log "display root"
-  curl $root
+  curl ${root}
 
   a-log "add letter groo001-1539-01"
-  cwg-add-letter $groo001 "groo001-1539-01"
+  cwg-add-letter ${groo001} "groo001-1539-01"
   a-log "add letter huyg001-0035"
-  cwg-add-letter $huyg001 "huyg001-0035"
+  cwg-add-letter ${huyg001} "huyg001-0035"
   a-log "add letter huyg001-2976"
-  cwg-add-letter $huyg001 "huyg001-2976"
+  cwg-add-letter ${huyg001} "huyg001-2976"
   a-log "add letter huyg003-0052"
-  cwg-add-letter $huyg003 "huyg003-0052"
+  cwg-add-letter ${huyg003} "huyg003-0052"
   a-log "add letter huyg003-1376"
-  cwg-add-letter $huyg003 "huyg003-1376"
+  cwg-add-letter ${huyg003} "huyg003-1376"
 
   a-log "DONE"
 }
@@ -41,7 +41,7 @@ function cwg-upload {
 function cwg-create-root-resource {
   # because we PUT the resource, it has status CONFIRMED
   id=$1
-  curl -i -X PUT $be/resources/$id --header "${authheader}" --header 'Content-type: application/json' \
+  curl -i -X PUT ${be}/resources/${id} --header "${authheader}" --header 'Content-type: application/json' \
   --data-binary "{\"resource\":{
     \"id\":\"$id\",
     \"ref\":\"CWG-corpus\"
@@ -63,7 +63,7 @@ function cwg-set-text-from-file {
 # usage: cwg-add-letter corpus-resource letter-id
 function cwg-add-letter {
   cwg-create-subresource $1 $2
-  curl $suburi
-  cwg-set-text-from-file $suburi "$2.xml"
-  curl $suburi/text
+  curl ${suburi}
+  cwg-set-text-from-file ${suburi} "$2.xml"
+  curl ${suburi}/text
 }
